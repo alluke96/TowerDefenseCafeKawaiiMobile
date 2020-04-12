4cc44fea0bbdbed149067405c0f66b297ad4d9f6
Allyson Luan Dunke <alluke.pucpr@gmail.com>
2020-04-12 03:02:10 -0300

    removed Joystick / added unitypackage v0.1

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    //-----------------------------------------------------------------------------------------------------------------
+    // Public methods
+    //-----------------------------------------------------------------------------------------------------------------
+    public void SceneLoader(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
